1da7eb48457f30a6d96f172804af52db3cab445c
Augusto002 <henrique.augustob27@gmail.com>
2021-07-31 12:39:35 -0300

    Implementacao do model, View, e Controller.

--- src/main/java/com/example/APENASESTUDANDO/EstudoSpringBoot/HomeController.java
+++ src/main/java/com/example/APENASESTUDANDO/EstudoSpringBoot/HomeController.java
@@ -2,13 +2,17 @@ package com.example.APENASESTUDANDO.EstudoSpringBoot;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
+@Controller  //É uma anotação para que o compilador entenda que a classe é um Controller;
 public class HomeController {
 
-    @RequestMapping("/")
-    public String HomeAPP(){
+    @RequestMapping("/")  //indica o caminho do endereço onde está a ser executado a aplicação //Ponto de entrada da aplicação
+    public String HomeAPP(Model model){   //Esse metodo vai chamar a classe html= index;
+        model.addAttribute("mensagem","OI Testando"); // Para enviar alguma valor usa o Model para a classe index
+
+
         return "index";
     }
 
